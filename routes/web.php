177570d70f402760ad0089577e5e82e59a3c9ff6
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2024-04-10 23:48:32 +0900

    Configure routes.

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Web\Auth\Controllers\LoginController;
 use App\Http\Web\Auth\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', LoginController::class)->name('web.login');
+Route::post('login', LoginController::class)->name('login');
 Route::post('logout', LogoutController::class)->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('welcome');
-    })->name('web.home');
+    })->name('home');
 });
