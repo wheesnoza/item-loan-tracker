b292b7442b1fc98dbe2cc18d0894491429ee4e3d
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2024-04-10 19:13:45 +0900

    Implement request of items.

---
+++ app/Core/Request/Actions/PerformItemsRequestAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Core\Request\Actions;
+
+use App\Core\Request\Models\Request;
+use App\Core\User\Models\User;
+
+class PerformItemsRequestAction
+{
+    public static function execute(User $performer, string $reason, array $itemIds): void
+    {
+        /** @var Request $request */
+        $request = $performer->requests()->create(['reason' => $reason]);
+
+        $request->items()->attach($itemIds);
+    }
+}
