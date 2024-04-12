83bec1d06aeb4d9c4730f253aa2e79e0435e95f0
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2024-04-12 20:13:31 +0900

    Append properties doc to model. And remove unnecessary factory trait.

--- app/Core/User/Models/User.php
+++ app/Core/User/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Core\User\Models;
 use App\Core\Item\Models\Item;
 use App\Core\Item\Models\LoanedItem;
 use App\Core\Request\Models\Request;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -13,12 +12,14 @@ use Illuminate\Notifications\Notifiable;
 
 /**
  * @property int $id
+ * @property string $first_name
+ * @property string $last_name
  * @property string $email
  * @property string $password
  */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     protected $fillable = ['first_name', 'last_name', 'email', 'password'];
 
